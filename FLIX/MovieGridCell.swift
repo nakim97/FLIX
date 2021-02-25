2d6a920f0a658890431edf0119e7cb0bd5989da0
Na Kim <nak001@ucsd.edu>
2021-02-25 16:01:54 -0500

    Update Collection View Setup

---
+++ FLIX/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FLIX
+//
+//  Created by Stella Kim on 2/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
